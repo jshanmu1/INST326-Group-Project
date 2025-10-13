0ccb47c406224422c4bd2ae3833149636a8f277f
jaywill67 <jviator@terpmail.umd.edu>
2025-10-12 20:59:42 -0400

    Update tmdb_functions.py
    add load_db_api_key and the comments describiing it

--- tmdb_functions.py
+++ tmdb_functions.py
@@ -1,3 +1,16 @@
 import requests
 import csv
 
+def load_db_api_key(path):
+    """
+    Read a TMDB API key from a text file.
+
+    Arguments: path is a string: Path to the text file that has the key.
+
+    Returns: An API key.
+
+    Possible Errors:
+        TypeError: If path is not a string.
+        FileNotFoundError: If the file does not exist.
+        ValueError: If the file is empty.
+    """
